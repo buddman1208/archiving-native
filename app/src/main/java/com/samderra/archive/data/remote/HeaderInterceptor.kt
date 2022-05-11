b98ae62ab5909e2fba8b431cdbeb27a5c5cd96da
Junseok Oh <buddman1208@gmail.com>
2022-05-11 13:14:13 +0900

    HeaderInterceptor.kt: make http request send user token

--- app/src/main/java/com/samderra/archive/data/remote/HeaderInterceptor.kt
+++ app/src/main/java/com/samderra/archive/data/remote/HeaderInterceptor.kt
@@ -1,12 +1,13 @@
 package com.samderra.archive.data.remote
 
+import com.samderra.archive.data.local.pref.PreferenceManager
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class HeaderInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val builder = chain.request().newBuilder()
-//        val token = PreferencesController.userInfoPref.token
+        val token = PreferenceManager.userPref.token
         builder.removeHeader("token")
         builder.addHeader("x-auth-token", "token")
         builder.removeHeader("x-platform")
