92810892385b752571f54eb9a27d74e48f34981e
Junseok Oh <buddman1208@gmail.com>
2022-05-20 01:59:47 +0900

    Article: match naming with remote

--- app/src/main/java/com/samderra/archive/ui/model/article/SDRArticle.kt
+++ app/src/main/java/com/samderra/archive/ui/model/article/SDRArticle.kt
@@ -6,18 +6,18 @@ data class SDRArticle(
     val articleId: Long,
     val thumbnailUrl: String,
     val linkUrl: String,
-    val title: String = "",
-    val content: String,
-    val date: Date
+    val name: String = "",
+    val description: String,
+    val createdAt: Date
 ) {
     companion object {
         fun dummy(): SDRArticle = SDRArticle(
             articleId = System.currentTimeMillis(),
             thumbnailUrl = "https://s3.orbi.kr/data/file/united/91a82be58e2eca9ea11f7652104b6304.jpg",
             linkUrl = "https://mathflat.com",
-            title = "내일부터 갓생을 살겠습니다",
-            content = "의지는 있나요? 의지는 있나요? 의지는 있나요? 의지는 있나요? 의지는 있나요? 의지는 있나요?",
-            date = Date(System.currentTimeMillis())
+            name = "내일부터 갓생을 살겠습니다",
+            description = "의지는 있나요? 의지는 있나요? 의지는 있나요? 의지는 있나요? 의지는 있나요? 의지는 있나요?",
+            createdAt = Date(System.currentTimeMillis())
         )
     }
 }
