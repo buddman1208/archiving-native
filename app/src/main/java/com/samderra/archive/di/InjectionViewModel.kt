a3f8e37fd5133e9ba2501f534a50daf9bb1812e9
Junseok Oh <buddman1208@gmail.com>
2022-05-20 01:58:50 +0900

    make injection for CategoryViewModel

--- app/src/main/java/com/samderra/archive/di/InjectionViewModel.kt
+++ app/src/main/java/com/samderra/archive/di/InjectionViewModel.kt
@@ -8,6 +8,6 @@ import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModel { MainViewModel(get(), get()) }
-    viewModel { CategoryViewModel() }
+    viewModel { CategoryViewModel(get()) }
     viewModel { ArticleViewModel() }
 }
